bfa3496acc74aad19eb226470296339d4d968b83
Khushi-Verma <44372851+Khushi-Verma@users.noreply.github.com>
2020-08-18 23:26:14 +0530

    Updates

---
+++ precache-manifest.584b1edac6737664c5217167cb55b86c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a743154c985dd6273954059290750b50",
+    "url": "/reactJS/index.html"
+  },
+  {
+    "revision": "cc3937b00d2e59a7adb4",
+    "url": "/reactJS/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e5f4482b3d8911c9606f",
+    "url": "/reactJS/static/js/2.dfe28b08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactJS/static/js/2.dfe28b08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc3937b00d2e59a7adb4",
+    "url": "/reactJS/static/js/main.b030e236.chunk.js"
+  },
+  {
+    "revision": "d109538064732933c0f5",
+    "url": "/reactJS/static/js/runtime-main.2491a4b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactJS/static/media/logo.5d5d9eef.svg"
+  }
+]);
