72e4ff99a9883c0614fd58db4afa6a83a8c64d9b
khushiverma <44372851+Khushi-Verma@users.noreply.github.com>
2020-09-09 12:50:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,9 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <>
+  <h1>khushi</h2>
+  </>,
   document.getElementById('root')
 );
 
